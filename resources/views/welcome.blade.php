e64b7eb597e9df09d7d7f0a3fc134245aa163c1f
Jānis <janisnamejs@inbox.lv>
2026-05-26 11:08:39 +0300

    dd

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>Laravel</title>
+  </head>
+  <body>
+    <h1>Sveiks, Laravel!</h1>
+  </body>
+</html>
---
+++ resources/views/why.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>adadad</title>
+  </head>
+  <body>
+    <h1>dadada!</h1>
+  </body>
+</html>
